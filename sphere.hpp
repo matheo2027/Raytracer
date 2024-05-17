7c5673125e33503a2777768fe9a7526dc2bed369
Mayard Mathieu <mathieu.mayard@epitech.eu>
2024-05-17 15:16:58 +0200

    ADD [sphere and plane]

---
+++ sphere.hpp
@@ -0,0 +1,52 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable_list.hpp"
+#include "vec3.hpp"
+
+class sphere : public hittable {
+public:
+    sphere(const point3& center, double radius) : center(center), radius(radius) {}
+
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        vec3 oc = r.origin() - center;
+        auto a = r.direction().length_squared();
+        auto half_b = dot(oc, r.direction());
+        auto c = oc.length_squared() - radius * radius;
+        auto discriminant = half_b * half_b - a * c;
+
+        if (discriminant < 0)
+            return false;
+
+        auto sqrtd = sqrt(discriminant);
+
+        auto root = (-half_b - sqrtd) / a;
+        if (root < ray_tmin || ray_tmax < root) {
+            root = (-half_b + sqrtd) / a;
+            if (root < ray_tmin || ray_tmax < root)
+                return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+
+        if (dot(r.direction(), outward_normal) > 0.0) {
+            rec.normal = -outward_normal;
+            rec.front_face = false;
+        } else {
+            rec.normal = outward_normal;
+            rec.front_face = true;
+        }
+
+        return true;
+    }
+
+
+private:
+    point3 center;
+    double radius;
+};
+
+#endif
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf build
-rm -rf lib/*.so
-rm -rf lib/*.a
-rm -rf raytracer
-
-cmake -S . -B build
-cmake --build build -- -j $(nproc --all)
